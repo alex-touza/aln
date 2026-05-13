1cdf1f26ff42ba5a9de43fd66e05a5922bbd6d92
Àlex Xavier Touza Luque <alex.touza.l@gmail.com>
2026-05-14 00:08:15 +0200

    Afegir variants inversa i desplaçada; resu

--- calcul_veps.py
+++ calcul_veps.py
@@ -1,18 +1,65 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional, Generic, TypeVar, override
 
 import numpy as np
 
+from metodes_directes import FactoritzacioLUParcialEsglaonat
+
 
 @dataclass
 class ResultatMetodePotencia:
     vep_associat: np.ndarray
+    """
+    Vector propi associat al valor propi de mòdul més gran.
+    """
     vap_dominant: np.floating
+    """
+    Valor propi de mòdul màxim.
+    """
+
+
+@dataclass
+class ResultatMetodePotenciaInversa:
+    vep_associat: np.ndarray
+    """
+    Vector propi associat al valor propi de mòdul mínim.
+    """
+    vap_minim: np.floating
+    """
+    Valor propi de mòdul mínim.
+    """
+
+
+@dataclass
+class ResultatMetodePotenciaInversaDesplacada:
+    vep_associat: np.ndarray
+    """
+    Vector propi associat al valor propi més proper al paràmetre q.
+    """
+    vap_mes_proper: np.floating
+    """
+    Valor propi més proper al paràmetre q.
+    """
+
+
+T = TypeVar("T")
+"""
+Dataclass que retorna
+"""
 
 
 class EstatMetodePotencia:
+    """
+    Estat per als mètodes de la potència i la potència inversa.
+
+    Nota: Si bé en els apunts de l'assignatura el mètode de la potència inversa fa servir x_k
+    en lloc de z_k, per simplificar el codi mantenim el nom de la variable com z_k.
+    """
+
     def __init__(self, A: np.ndarray, z0: np.ndarray):
         self.A = A
+        self.n = A.shape[0]
         self.z_: np.ndarray = z0
         self.z_prev: Optional[np.ndarray] = None
         self.y: np.ndarray = np.array([])
@@ -30,28 +77,62 @@ class EstatMetodePotencia:
         self.z_prev = self.z_.copy()
         self.z_ = z
 
-    def resultat(self):
-        return ResultatMetodePotencia(self.y, np.linalg.norm(self.y))
+
+class EstatMetodePotenciaInversa(EstatMetodePotencia):
+    def __init__(self, A: np.ndarray, z0: np.ndarray):
+        super().__init__(A, z0)
+        self.factoritzacio = FactoritzacioLUParcialEsglaonat(A)
+        self.factoritzacio.factoritzar()
+
+    def resoldre(self) -> np.ndarray:
+        """
+        Resol el sistema lineal Ay = z amb factorització LU amb pivotatge parcial esglaonat i en
+        retorna la solució y.
+        """
+        assert self.factoritzacio is not None
+        self.factoritzacio.resoldre(self.z)
+        assert self.factoritzacio.x is not None
+        return self.factoritzacio.x
 
 
-TEstat = TypeVar('TEstat', bound=EstatMetodePotencia)
+TResultat = TypeVar("TResultat")
+TEstat = TypeVar("TEstat", bound=EstatMetodePotencia)
 
 
-class MetodePotencia:
+class MetodePotenciaBase(Generic[TEstat, TResultat], ABC):
     def __init__(self, epsilon: float):
         self.epsilon = epsilon
-        self.estat: Optional[EstatMetodePotencia] = None
+        self.estat: Optional[TEstat] = None
 
-    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> EstatMetodePotencia:
-        return EstatMetodePotencia(A, z0)
-    
+    @abstractmethod
+    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> TEstat:
+        """
+        Retorna un objecte de tipus TEstat, que és derivat de EstatMetodePotenciaBase,
+        que representa l'estat inicial per a l'execució del mètode la potència o alguna
+        de les seves variants.
+
+        :param A: Matriu
+        :param z0: Aproximació inicial
+        :return: Retorna un objecte que representa l'estat inicial.
+        """
+        pass
+
+    @abstractmethod
     def aproximar(self) -> np.ndarray:
         """
         A partir de l'estat actual del mètode self.estat, retorna una tupla
         amb la nova aproximació de vector propi y.
         """
-        assert self.estat is not None
-        return self.estat.A @ self.estat.z
+        pass
+
+    @abstractmethod
+    def resultat(self) -> TResultat:
+        """
+        Mètode abstracte que cal implementar a les subclasses.
+
+        :return: Retorna el resultat calculat en una instància de tipus `TResultat`.
+        """
+        pass
 
     def calcular(self, A: np.ndarray, z0: np.ndarray):
         assert np.linalg.norm(z0, ord=np.inf) == 1
@@ -66,16 +147,88 @@ class MetodePotencia:
 
             seguir = self.estat.residu() > self.epsilon
 
-        return self.estat.resultat()
-    
+        return self.resultat()
 
-class MetodePotenciaInversa(MetodePotencia):
+
+class MetodePotencia(MetodePotenciaBase[EstatMetodePotencia, ResultatMetodePotencia]):
     @override
-    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> np.ndarray:
-        # Resoldre Ay = z amb LU i retornar y 
+    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> EstatMetodePotencia:
+        return EstatMetodePotencia(A, z0)
+
+    @override
+    def resultat(self) -> TResultat:
+        assert self.estat is not None
+        return ResultatMetodePotencia(self.estat.y, np.linalg.norm(self.estat.y))
+
+    @override
+    def aproximar(self) -> np.ndarray:
+        assert self.estat is not None
+        return self.estat.A @ self.estat.z
+
+
+class MetodePotenciaInversa(MetodePotenciaBase[EstatMetodePotenciaInversa, ResultatMetodePotenciaInversa]):
+    @override
+    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> EstatMetodePotenciaInversa:
+        return EstatMetodePotenciaInversa(A, z0)
+
+    @override
+    def resultat(self) -> ResultatMetodePotenciaInversa:
+        assert self.estat is not None
+        return ResultatMetodePotenciaInversa(self.estat.y, 1 / np.linalg.norm(self.estat.y))
+
+    @override
+    def aproximar(self) -> np.ndarray:
+        assert self.estat is not None
+
+        # Resoldre Ay = z amb LU i retornar y
+        self.estat.factoritzacio.resoldre(self.estat.z)
+
+        assert self.estat.factoritzacio.x is not None
+        return self.estat.factoritzacio.x  # y
+
+
+class MetodePotenciaDesplacada(MetodePotencia):
+    def __init__(self, epsilon: float, q: float):
+        super().__init__(epsilon)
+        self.q = q
+
+    @override
+    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> EstatMetodePotencia:
+        return EstatMetodePotencia(A - self.q * np.eye(A.shape[0]), z0)
+
+    @override
+    def resultat(self) -> ResultatMetodePotencia:
+        resultat_ = super().resultat()
+        return ResultatMetodePotencia(resultat_.vep_associat, resultat_.vap_dominant + self.q)
+
+
+class MetodePotenciaInversaDesplacada(MetodePotenciaInversa):
+    def __init__(self, epsilon: float, q: float):
+        super().__init__(epsilon)
+        self.q = q
+
+    @override
+    def inicialitzar_estat(self, A: np.ndarray, z0: np.ndarray) -> EstatMetodePotenciaInversa:
+        return EstatMetodePotenciaInversa(A - self.q * np.eye(A.shape[0]), z0)
+
+    @override
+    def resultat(self) -> ResultatMetodePotenciaInversa:
+        """
+        Aquí el tipus retornat és diferent que en MetodePotenciaInversa. Això trenca el principi de
+        substitució de Liskov, però com a la pràctica només farem servir aquestes classes directament
+        i no a través de classes parents, ho passarem per alt.
+
+        :return: Un objecte de tipus ResultatMetodePotenciaInversaDesplacada que indica el valor propi
+        més proper al paràmetre q i el vector propi associat a aquest valor propi.
+        """
+        resultat_ = super().resultat()
+        return ResultatMetodePotenciaInversa(resultat_.vep_associat, 1 / (resultat_.vap_minim + self.q))
+
+
+
 
 metode = MetodePotencia(1e-8)
-metode.calcular(np.array([
+resultat = metode.calcular(np.array([
     [1, 1, 1, 1],
     [1, 2, 1, 1],
     [1, 1, 3, 1],
@@ -87,8 +240,7 @@ metode.calcular(np.array([
     [1]
 ]
 )))
-
-
+print(resultat.vap, resultat.vep_associat)
 
 assert metode.estat is not None
 
